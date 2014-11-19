9520baeb3ea04e6f02e227e1ed9d8be1a614d794
Makis Tracend <makis.tracend@gmail.com>
2014-11-18 16:56:01 -0800

    :up: build script

--- build.js
+++ build.js
@@ -1,42 +1,58 @@
+// # Build script using Node.js
+//
+// Single script build,used as a lightweight alternative
+// when a build platform (grunt/gulp wtc.) feels like overkill
+//
+// - Dependencies: NPM/Node.js
+// - Conventions:
+// * code is in a lib/ folder with a main.js as the main context (closure)
+// * a bower.json on the root contains all the info about the lib: name, description, author, license
+// * compiled files are saved in a build folder
+
 // settings
 var FILE_ENCODING = 'utf-8',
 	EOL = '\n';
 
 // Dependencies
-var _cli = require('commander'),
-	_uglify = require("uglify-js"),
-	_jshint = require('jshint'),
-	_handlebars = require('hbs'),
-	_fs = require('fs');
+var cli = require('commander'),
+	uglify = require("uglify-js"),
+	jshint = require('jshint'),
+	handlebars = require('hbs'),
+	fs = require('fs'),
+	zlib = require('zlib');
 
 
-// will generate a CSV if package.json contains multiple licenses
-_handlebars.registerHelper('license', function(items){
+// will generate a CSV if package info contains multiple licenses
+/*
+handlebars.registerHelper('license', function(items){
 	items = items.map(function(val){
 		return val.type;
 	});
 	return items.join(', ');
 });
-
+*/
 
 // Logic
+// - read module name from bower file
+var package = JSON.parse( fs.readFileSync('bower.json', FILE_ENCODING) ); // condition the existance of package.json or component.json...
+var name = package.name;
+// - list files in the lib folder
+var src = libFiles();
+// add components
+src.push('bower_components/easing/easing.js', 'bower_components/tick/tick.js');
+
 // - concatinate all files
 concat({
-	src : [
-		'lib/view.js',
-		'lib/helpers.js',
-		'bower_components/easing/easing.js',
-		'bower_components/tick/tick.js'
-	],
-	dest : 'build/backbone.easing.js'
+	src: src,
+	dest: 'build/'+ name +'.js'
 });
 
 
 // - Validate js
-lint('build/backbone.easing.js', function(){
+lint('build/'+ name +'.js', function(){
 
 	// - Create / save minified file
-	uglify('build/backbone.easing.js', 'build/backbone.easing-min.js');
+	minify('build/'+ name +'.js', 'build/'+ name +'-min.js');
 
 });
 
@@ -48,59 +64,64 @@ function concat(opts) {
 	var distPath = opts.dest;
 
 	var lib = fileList.map(function(filePath){
-			return _fs.readFileSync(filePath, FILE_ENCODING);
+			return fs.readFileSync(filePath, FILE_ENCODING);
 		});
 
-	var wrapper = _fs.readFileSync('lib/main.js', FILE_ENCODING);
+	var wrapper = fs.readFileSync('lib/main.js', FILE_ENCODING);
 
-	var template = _handlebars.compile( wrapper );
+	var template = handlebars.compile( wrapper );
 
 	//reuse package.json data and add build date
-	var data = JSON.parse( _fs.readFileSync('package.json', FILE_ENCODING) );
+	var data = package;
 	data.lib = lib.join(EOL);
 	data.build_date = (new Date()).toUTCString();
 
 	// Save uncompressed file
-	_fs.writeFileSync(distPath, template(data), FILE_ENCODING);
+	fs.writeFileSync(distPath, template(data), FILE_ENCODING);
 	console.log(' '+ distPath +' built.');
 
 }
 
 
-function uglify(srcPath, distPath) {
+function minify(srcPath, distPath) {
 	/*
 	var
 	  jsp = uglyfyJS.parser,
 	  pro = uglyfyJS.uglify,
-	  ast = jsp.parse( _fs.readFileSync(srcPath, FILE_ENCODING) );
+	  ast = jsp.parse( fs.readFileSync(srcPath, FILE_ENCODING) );
 
 	ast = pro.ast_mangle(ast);
 	ast = pro.ast_squeeze(ast);
 	*/
 
-	var result = _uglify.minify(srcPath, { compressor: {
-		comments : true
+	var min = uglify.minify(srcPath, { compressor: {
+		comments : /@name|@author|@cc_on|@url|@license/
 	} });
 
-	_fs.writeFileSync(distPath, result.code, FILE_ENCODING);
-	console.log(' '+ distPath +' built.');
+	// gzip
+	zlib.gzip(min.code, function (error, result) {
+		if (error) throw error;
+		fs.writeFileSync(distPath, result, FILE_ENCODING);
+		console.log(' '+ distPath +' built.');
+	});
+
 }
 
 function lint(path, callback) {
-	var buf = _fs.readFileSync(path, 'utf-8');
+	var buf = fs.readFileSync(path, 'utf-8');
 	// remove Byte Order Mark
 	buf = buf.replace(/^\uFEFF/, '');
 
-	_jshint.JSHINT(buf);
+	jshint.JSHINT(buf);
 
-	var nErrors = _jshint.JSHINT.errors.length;
+	var nErrors = jshint.JSHINT.errors.length;
 
 	if (nErrors) {
 		// ruff output of errors (for now)
-		console.log(_jshint.JSHINT.errors);
+		console.log(jshint.JSHINT.errors);
 		console.log(' Found %j lint errors on %s, do you want to continue?', nErrors, path);
 
-		_cli.choose(['no', 'yes'], function(i){
+		cli.choose(['no', 'yes'], function(i){
 			if (i) {
 				process.stdin.destroy();
 				if(callback) callback();
@@ -111,4 +132,17 @@ function lint(path, callback) {
 	} else if (callback) {
 		callback();
 	}
+}
+
+function libFiles(){
+	var src = [];
+	var files = fs.readdirSync( "lib/" );
+	// folter only javascript files
+	for( var i in files ){
+		var file = files[i];
+		// exclude certain files and main.js
+		if( file.substr(0, 1) == "." || file.substr(-3) !== ".js" || file == "main.js" ) continue;
+		src.push( "lib/"+ file );
+	}
+	return src;
 }
